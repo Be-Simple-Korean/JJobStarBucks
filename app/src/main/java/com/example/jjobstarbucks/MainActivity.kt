87554fc91a8a47e2f1607f7d7d65c599807803b6
이정현 <cw03125@openobject.net>
2023-06-01 17:47:06 +0900

    add navigation fragment & bottom navigation view

--- app/src/main/java/com/example/jjobstarbucks/MainActivity.kt
+++ app/src/main/java/com/example/jjobstarbucks/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.jjobstarbucks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.jjobstarbucks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(androidx.navigation.fragment.R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnv.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/jjobstarbucks/fragment/OrderFragment.kt
@@ -0,0 +1,30 @@
+package com.example.jjobstarbucks.fragment
+
+import android.os.Binder
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.jjobstarbucks.R
+import com.example.jjobstarbucks.databinding.FragmentOrderBinding
+
+class OrderFragment : Fragment(R.layout.fragment_order) {
+
+    private lateinit var binding: FragmentOrderBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentOrderBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+
+}
