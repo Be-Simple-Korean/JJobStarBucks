a0aab156e485dbd62af6dd61c4ad54c3ebbb222c
이정현 <cw03125@openobject.net>
2023-06-02 16:37:21 +0900

    home app bar ui

--- app/src/main/java/com/example/jjobstarbucks/MainActivity.kt
+++ app/src/main/java/com/example/jjobstarbucks/MainActivity.kt
@@ -14,8 +14,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(androidx.navigation.fragment.R.id.nav_host_fragment_container) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnv.setupWithNavController(navController)
     }
